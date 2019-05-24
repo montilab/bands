3b03c2130bb4fa1129370655b695f9a19f777378
Anthony Federico <anfederico@users.noreply.github.com>
2019-05-24 13:44:12 -0400

    Initial version

---
+++ bands.R
@@ -0,0 +1,83 @@
+library(plotly)
+
+# Example data
+samples <- 200
+chromosomes <- c(paste('chr', 1:21, sep=""))
+starts <- sample(1:500000, samples, replace=T)
+ends <- sapply(starts, function(x) x+sample(1:10000, 1))
+names <- sample(LETTERS, samples, replace=T)
+colors <- sample(c("red", "green", "blue", "purple", "orange"), samples, replace=T)
+
+df = data.frame(band  = sample(chromosomes, 100, replace=T),
+                start = starts,
+                end   = ends,
+                name  = names,
+                color = colors,
+                stringsAsFactors=FALSE)
+
+head(df)
+# ------
+
+# Helpers
+new.segment <- function(x0, x1, y0, y1, name='', color='black', opacity=0.8) {
+    return(list(x0=x0, x1=x1, xref='x',
+                y0=y0, y1=y1, yref='y',
+                type='rect',
+                fillcolor=color, 
+                line=list(color=color), 
+                opacity=opacity,
+                hoveron='area',
+                hoverinfo=name,
+                layer='above'))
+}
+# ------
+
+# Formulate segmants
+bands <- chromosomes # sort(unique(df$band)) # Can replace bands with any ordered vector of bands
+bands.findy <- setNames(seq(1, length(bands)*2, by=2), bands)
+min.start <- min(df$start)
+
+shapes <- apply(df, 1, function(x) {
+    y0 = as.integer(bands.findy[x[1]])
+    y1 = y0 + 1
+    x0 = x[2]
+    x1 = x[3]
+    name = x[4]
+    color = x[5]
+    if (!is.na(y0)) {
+        new.segment(x0, x1, y0, y1, name, color)
+    }
+})
+# ------
+
+# Band labeling
+annotations <- list(x = rep(min.start, length(bands.findy)),
+                    y = as.integer(bands.findy)+0.5,
+                    text = names(bands.findy),
+                    xref = 'x',
+                    yref = 'y',
+                    borderpad = 10,
+                    xanchor = 'right',
+                    showarrow = FALSE)
+# ------
+
+# Plotting
+p <- layout(plotly_empty(type = 'area'),
+            shapes = shapes,
+            annotations = annotations,
+            autosize = T,
+            margin = list(l=5, r=5, b=5, t=10, pad=0)) %>%
+      
+     add_trace(x = (df$start+df$end)/2, 
+               y = as.integer(bands.findy[df$band])+0.5, 
+               type="scatter",
+               mode="markers",
+               opacity = 0,
+               text = df$name,
+               hoveron = 'points',
+               hoverinfo = 'text',
+               hoverlabel = list(bgcolor='white', font=c(color='black')),
+               showlegend = FALSE)
+
+p
+# ------
