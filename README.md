bc70865f5b9836ca36e1174bf307f368c80fa58a
Anthony Federico <anfederico@users.noreply.github.com>
2019-05-25 11:07:39 -0400

    Generalization and examples

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # bands
+
 Simple interactive chromosome maps written in R
+
+## Source
+
+``` r
+source("bands.R")
+```
+
+## Example Data
+
+``` r
+samples <- 250
+bands <- paste('Band', 0:9)
+starts <- sample(1:500000, samples, replace=TRUE)
+ends <- sapply(starts, function(x) x+sample(1:10000, 1))
+names <- paste('Label', sample(LETTERS, samples, replace=TRUE))
+colors <- sample(c('#f3cec9', '#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844'), samples, replace=T)
+opacitys <- sample(50:100/100, samples, replace=TRUE)
+
+df <- data.frame(band = sample(bands, samples, replace=TRUE),
+                 start = starts,
+                 end = ends,
+                 name = names,
+                 color = colors,
+                 opacity = opacitys,
+                 stringsAsFactors=FALSE)
+
+dim(df)
+```
+
+    [1] 250   6
+
+``` r
+head(df)
+```
+
+``` 
+    band  start    end    name   color opacity
+1 Band 4 309267 310144 Label I #6f4d96    0.96
+2 Band 7  24655  28810 Label D #182844    0.68
+3 Band 9 359082 363749 Label Q #a262a9    0.72
+4 Band 1 141285 143092 Label Y #182844    0.72
+5 Band 4  24444  26772 Label I #6f4d96    0.71
+6 Band 9 409560 414910 Label R #6f4d96    0.50
+```
+
+## Plot Bands
+
+``` r
+draw.bands(df)
+```
+
+![](man/figures/README-unnamed-chunk-4-1.png)<!-- -->
+
+## Real Data Interactive Examples
+
+Please visit <https://anfederico.github.io/bands/>
--- bands.R
+++ bands.R
@@ -1,39 +1,5 @@
 library(plotly)
 
-# Example data
-samples <- 100
-chromosomes <- c(paste('chr', 1:21, sep=""))
-starts <- sample(1:500000, samples, replace=T)
-ends <- sapply(starts, function(x) x+sample(1:10000, 1))
-names <- sample(LETTERS, samples, replace=T)
-colors <- sample(c("red", "green", "blue", "purple", "orange"), samples, replace=T)
-
-df = data.frame(band  = sample(chromosomes, 100, replace=T),
-                start = starts,
-                end   = ends,
-                name  = names,
-                color = colors,
-                stringsAsFactors=FALSE)
-
-head(df)
-# ------
-
-# Real Data
-chromosomes <- c(paste("chr", 1:21, sep=""), "chrX", "chrY", "chrM")
-df <- read.table('data/hg38.txt', sep="\t", header=TRUE, stringsAsFactors=FALSE)
-
-# Randomly reduce the size 
-df <- df[sample(1:nrow(df), 500, replace=FALSE),]
-df$color = "red"
-df[sample(1:nrow(df), 100, replace=FALSE), "color"] <- "gold"
-colnames(df) <- c("band", "name", "start", "end", "color")
-df = df[,c("band", "start", "end", "name", "color")]
-rownames(df) <- NULL
-
-head(df)
-# ------
-
-# Helpers
 new.segment <- function(x0, x1, y0, y1, name='', color='black', opacity=0.8) {
     return(list(x0=x0, x1=x1, xref='x',
                 y0=y0, y1=y1, yref='y',
@@ -45,57 +11,65 @@ new.segment <- function(x0, x1, y0, y1, name='', color='black', opacity=0.8) {
                 hoverinfo=name,
                 layer='above'))
 }
-# ------
 
-# Ensure
-rownames(df) <- NULL
-
-# Formulate segmants
-bands <- chromosomes # sort(unique(df$band)) # Can replace bands with any ordered vector of bands
-bands.findy <- setNames(seq(1, length(bands)*2, by=2), bands)
-min.start <- min(df$start)
-
-shapes <- apply(df, 1, function(x) {
-    y0 = as.integer(bands.findy[x[1]])
-    y1 = y0 + 1
-    x0 = x[2]
-    x1 = x[3]
-    name = x[4]
-    color = x[5]
-    if (!is.na(y0)) {
-        new.segment(x0, x1, y0, y1, name, color)
+draw.bands <- function(df, bands=NULL, labels=FALSE) {
+    
+    # Ensure for apply function
+    rownames(df) <- NULL
+    
+    # Sorted bands to include in plot
+    if (is.null(bands)) {
+        bands <- sort(unique(df$band))
     }
-})
-# ------
-
-# Band labeling
-annotations <- list(x = rep(min.start, length(bands.findy)),
-                    y = as.integer(bands.findy)+0.5,
-                    text = names(bands.findy),
-                    xref = 'x',
-                    yref = 'y',
-                    borderpad = 10,
-                    xanchor = 'right',
-                    showarrow = FALSE)
-# ------
-
-# Plotting
-p <- layout(plotly_empty(type = 'area'),
-            shapes = shapes,
-            annotations = annotations,
-            autosize = T,
-            margin = list(l=5, r=5, b=5, t=10, pad=0)) %>%
+    
+    bands.findy <- setNames(seq(1, length(bands)*2, by=2), bands)
+    min.start <- min(df$start)    
+    
+    # Band segments
+    shapes <- apply(df, 1, function(x) {
+        y0      = as.integer(bands.findy[x[1]])
+        y1      = y0 + 1
+        x0      = x[2]
+        x1      = x[3]
+        name    = ifelse(is.na(x[4]), "", x[4])
+        color   = ifelse(is.na(x[5]), "black", x[5])
+        opacity = ifelse(is.na(x[6]), 0.8, x[6])
+        
+        if (!is.na(y0)) {
+            new.segment(x0, x1, y0, y1, name, color, opacity)
+        }
+    })
+    
+    # Band labels on y-axis
+    annotations <- list(x = rep(min.start, length(bands.findy)),
+                        y = as.integer(bands.findy)+0.5,
+                        text = names(bands.findy),
+                        xref = 'x',
+                        yref = 'y',
+                        borderpad = 10,
+                        xanchor = 'right',
+                        showarrow = FALSE)
+    
+    # Draw shapes
+    p <- layout(plotly_empty(type = 'area'),
+                shapes = shapes,
+                annotations = annotations,
+                autosize = T,
+                margin = list(l=5, r=5, b=5, t=10, pad=0))
       
-     add_trace(x = (df$start+df$end)/2, 
-               y = as.integer(bands.findy[df$band])+0.5, 
-               type="scatter",
-               mode="markers",
-               opacity = 0,
-               text = df$name,
-               hoveron = 'points',
-               hoverinfo = 'text',
-               hoverlabel = list(bgcolor='white', font=c(color='black')),
-               showlegend = FALSE)
-
-p
-# ------
+    # Hover info for shape names
+    if (labels) {
+        p <- add_trace(p,
+                       x = (df$start+df$end)/2, 
+                       y = as.integer(bands.findy[df$band])+0.5, 
+                       type="scatter",
+                       mode="markers",
+                       opacity = 0,
+                       text = df$name,
+                       hoveron = 'points',
+                       hoverinfo = 'text',
+                       hoverlabel = list(bgcolor='white', font=c(color='black')),
+                       showlegend = FALSE)
+    }
+    return(p)
+}
